33a83637ac371d4d7ecb28e5f8450d9cbad1c2bd
Serikpg <45354903+Serikpg@users.noreply.github.com>
2025-08-06 11:00:17 +0200

    Initial commit, creating simulator

---
+++ README.md
@@ -0,0 +1,2 @@
+# llangardaix
+This respository will try to serve as a performance and correctness model for the Sargantana core using the MAP Sparta simulation envioronment.
---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+
+#include "sparta/app/CommandLineSimulator.hpp"
+
+#include "Simulator.hpp"
+#include "CPUFactory.hpp"
+
+const char USAGE[] =
+    "Usage:\n"
+    "    [-i insts] [-r RUNTIME] [--show-tree] [--show-dag]\n"
+    "    [-p PATTERN VAL] [-c FILENAME]\n"
+    "    [--node-config-file PATTERN FILENAME]\n"
+    "    [-l PATTERN CATEGORY DEST]\n"
+    "    [-h]\n"
+    "\n";
+
+constexpr char VERSION_VARNAME[] = "0.1.0";
+
+int main(int argc, char **argv)
+{
+    sparta::app::DefaultValues DEFAULTS;
+    DEFAULTS.auto_summary_default = "on";
+
+    // create Sparta command-line simulator
+
+    sparta::app::CommandLineSimulator cls(USAGE, DEFAULTS);
+    auto& app_opts = cls.getApplicationOptions();
+    
+    // parse the command-line arguments and return error in this case
+    int err_code = 0;
+    if (!cls.parse(argc, argv, err_code))
+        return err_code;
+    
+    sparta::Scheduler scheduler;
+    Simulator sim(scheduler);
+
+    cls.populateSimulation(&sim);
+    cls.runSimulator(&sim);
+    cls.postProcess(&sim);
+
+    return 0;
+}
